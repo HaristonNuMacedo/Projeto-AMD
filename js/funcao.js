5ceba7da2d3bdf2e7e17b71f997c1e6fd142b9a5
HaristonNuMacedo <haristonmacedo2001@gmail.com>
2021-06-08 10:24:41 -0300

    funcao

---
+++ js/funcao.js
@@ -0,0 +1,16 @@
+function helloWorld(){
+    document.write('Olá Mundo!');
+}
+
+helloWorld();
+
+function helloWorldName(name){
+    return document.write('<br> OLá Mundo, meu nome é ' + nome);
+}
+
+helloWorld('Ryzen 7');
+
+function calculaAreaRetangulo(base, altura){
+    var area = base * altura;
+}
+
--- js/operadoresAritmeticos.js
+++ js/operadoresAritmeticos.js
@@ -1,12 +1,25 @@
 var subtotal = (13+1)*5;
 var frete = 0.5*(13+1);
 var total = subtotal + frete;
+var cifrao = 'R$';
 
 var subResultado = document.getElementById('subtotalvalor');
-subResultado.textContent = 'R$' + subtotal;
+subResultado.textContent += cifrao + subtotal;
 
 var freteResultado = document.getElementById('fretevalor');
-freteResultado.textContent = 'R$' + frete;
+freteResultado.textContent += cifrao + frete;
 
 var totalResultado = document.getElementById('totalvalor');
-totalResultado.textContent = total;
+totalResultado.textContent += cifrao + total;
+
+
+
+var processador = {
+    nome: 'Ryzen 7 5800x',
+    arquitetura: 'Zen3',
+    ano: 2020,
+    serie: 5000
+}
+
+var subPorcessor = document.getElementById('processor')
+subProcessor += processador; 
