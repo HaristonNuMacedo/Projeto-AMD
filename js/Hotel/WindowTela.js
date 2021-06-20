33851a8a8d5be1b3571cf19351a903a89bdd13fa
HaristonNuMacedo <85318716+HaristonNuMacedo@users.noreply.github.com>
2021-06-20 19:49:15 -0300

    Update Hotel

--- js/Hotel/WindowTela.js
+++ js/Hotel/WindowTela.js
@@ -1,3 +1,5 @@
+
+// 1° Sessão do site
 function iniciaModal(modalID) {
 
     var modal = document.getElementById(modalID);
@@ -10,11 +12,57 @@ function iniciaModal(modalID) {
         });
     }
 }
-var logo = document.querySelector('.pst1');
+var logo = document.querySelector('.seasonSuite00');
 logo.addEventListener('click', () => iniciaModal('modal-controlador'));
 
 document.addEventListener('scroll', () => {
     if (window.pageYOffset > 2800) {
         iniciaModal('modal-controlador')
+    }
+})
+
+
+// 2° Sessão do site
+function iniciaModal(modalID) {
+
+    var modal = document.getElementById(modalID);
+    if (modal) {
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', (e) => {
+            if (e.target.id == modalID || e.target.className == 'fechar') {
+                modal.classList.remove('mostrar');
+            }
+        });
+    }
+}
+var logo = document.querySelector('.seasonSuite22');
+logo.addEventListener('click', () => iniciaModal('modal-controlador2'));
+
+document.addEventListener('scroll', () => {
+    if (window.pageYOffset > 2800) {
+        iniciaModal('modal-controlador2')
+    }
+})
+
+
+// 3° Sessão do site
+function iniciaModal(modalID) {
+
+    var modal = document.getElementById(modalID);
+    if (modal) {
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', (e) => {
+            if (e.target.id == modalID || e.target.className == 'fechar') {
+                modal.classList.remove('mostrar');
+            }
+        });
+    }
+}
+var logo = document.querySelector('.seasonSuite44');
+logo.addEventListener('click', () => iniciaModal('modal-controlador3'));
+
+document.addEventListener('scroll', () => {
+    if (window.pageYOffset > 2800) {
+        iniciaModal('modal-controlador3')
     }
 })
